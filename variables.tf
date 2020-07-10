bcd12189425e294b24082db51ac903381ee68e0d
William Hearn <sylus1984@gmail.com>
2020-07-10 12:25:41 -0400

    feat(var): Remove variables

--- variables.tf
+++ variables.tf
@@ -14,31 +14,3 @@ variable "values" {
   default = ""
   type = "string"
 }
-
-variable "kubecost_token" {
-  description = "The token to use for KubeCost"
-}
-
-variable "kubecost_domain_name" {
-  description = "The domain name to use for KubeCost"
-}
-
-variable "kubecost_cluster_name" {
-  description = "The cluster name to use for KubeCost"
-}
-
-variable "kubecost_subscription_id" {
-  description = "The subscription id to use for KubeCost"
-}
-
-variable "kubecost_client_id" {
-  description = "The client id to use for KubeCost"
-}
-
-variable "kubecost_tenant_id" {
-  description = "The tenant id to use for KubeCost"
-}
-
-variable "kubecost_product_key" {
-  description = "The product key to use for KubeCost"
-}
